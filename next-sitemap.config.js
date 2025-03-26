e66469b6b41c4648dce3de34bf0569bf66ca643c
tomoki013 <tomoki_ttttt@icloud.com>
2025-03-27 08:14:20 +0900

    サイトマップの修正

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://tomokichidiary.netlify.app/', // あなたのサイトのURLに置き換えてください
+    siteUrl: 'https://tomokichidiary.netlify.app/',
+    changefreq: 'weekly',
     generateRobotsTxt: true, // robots.txt の生成を有効にする場合
+    sitemapSize: 7000,
+    outDir: './out',
     // 他のオプション（例：exclude, changefreq, priority など）も必要に応じて追加できます
 }
