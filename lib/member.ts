7d87acd43a4b6191f55b51b4ecc2ddc17e081852
tomoki013 <tomoki_ttttt@icloud.com>
2025-05-10 22:31:43 +0900

    各種変更

--- lib/member.ts
+++ lib/member.ts
@@ -1,7 +1,7 @@
 export const members = [
     {
         name: 'ともきち',
-        role: '管理者, 旅行ブロガー',
+        role: '管理者, 執筆者',
         image: '/images/Introduce/introduce.jpg',
         description: '旅行好きの大学生。世界中の様々な文化を経験するために旅をしています。',
     },
