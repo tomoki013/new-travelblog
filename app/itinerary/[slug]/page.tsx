a34750ccbe27376c99b23b3f455c66278df68bc6
tomoki013 <tomoki_ttttt@icloud.com>
2025-06-07 20:44:04 +0900

    popup共通化の下準備、履歴残す用のコミット2

--- app/itinerary/[slug]/page.tsx
+++ app/itinerary/[slug]/page.tsx
@@ -95,7 +95,7 @@ const ItineraryPostPage = async (props: { params: Promise<{ slug: string }>}) =>
             <div className="grid gap-10 lg:grid-cols-3">
                 
                 <div className='lg:col-span-2'>
-                    <Elements.Popup buttonType='button' />
+                    <Elements.Popup itemType='posts?type=itinerary' buttonType='button' />
                     <Sections.Article
                         post={post}
                         author={author}
