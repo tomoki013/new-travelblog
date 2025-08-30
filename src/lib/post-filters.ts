9240f509238ab888f946a768b733cc61be3433e3
tomoki013 <tomoki_ttttt@icloud.com>
2025-08-31 02:02:25 +0900

    ブログページが表示されない重大インシデントの修正

--- src/lib/post-filters.ts
+++ src/lib/post-filters.ts
@@ -1,16 +1,25 @@
-import { Post } from '@/types/types';
+import { Post } from "@/types/types";
 
-type PostMetadata = Omit<Post, 'content'>;
+type PostMetadata = Omit<Post, "content">;
 
 export function sortByDate(posts: PostMetadata[]): PostMetadata[] {
-  return [...posts].sort((a, b) => (a.date < b.date ? 1 : -1));
+  return [...posts].sort((a, b) => {
+    // bの日付からaの日付を引くことで、新しいものが先頭に来るようにする
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
+  });
 }
 
-export function filterByType(posts: PostMetadata[], type: string): PostMetadata[] {
-  return posts.filter(post => post.type === type);
+export function filterByType(
+  posts: PostMetadata[],
+  type: string
+): PostMetadata[] {
+  return posts.filter((post) => post.type === type);
 }
 
-export function filterByTag(posts: PostMetadata[], tag: string): PostMetadata[] {
+export function filterByTag(
+  posts: PostMetadata[],
+  tag: string
+): PostMetadata[] {
   // `tags` is now guaranteed to be an array by the data layer.
-  return posts.filter(post => post.tags && post.tags.includes(tag));
+  return posts.filter((post) => post.tags && post.tags.includes(tag));
 }
